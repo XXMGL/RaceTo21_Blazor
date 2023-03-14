f8192d1bdf41feceeba9026c647eb717b14dfc9b
XXMGL <1056181445@qq.com>
2023-03-14 15:30:11 -0400

    1

---
+++ Rt21_Interface/Card.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace RaceTo21
+{
+    public class Card
+    {
+        public string id;//Shot name of the card
+        public string displayName;//full name of the card
+        public Card(string shortCardName,string longCardName)
+        {
+            id = shortCardName;
+            displayName = longCardName;
+        }
+    }
+}
